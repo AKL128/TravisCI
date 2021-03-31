ceb40f8206dcbc7406c8eadde618684c84adfaa1
AKL128 <anthonyluu128@gmail.com>
2021-03-31 07:40:27 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -32,7 +32,7 @@ namespace TravisCILab
 
         // Implement 3 tests per operation, following a similar pattern as above
         [Test]
-        public void Subtract_Valid()
+        public void Subtract_Valid_Luu()
         {
             Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(5, Program.Subtract("7", "2"));
@@ -40,7 +40,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Subtract_Invalid()
+        public void Subtract_Invalid_Luu()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
@@ -48,7 +48,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Subtract_Null()
+        public void Subtract_Null_Luu()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -56,7 +56,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Multiply_Valid()
+        public void Multiply_Valid_Luu()
         {
             Assert.AreEqual(6, Program.Multiply("3", "2"));
             Assert.AreEqual(14, Program.Multiply("7", "2"));
@@ -64,7 +64,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Multiply_Invalid()
+        public void Multiply_Invalid_Luu()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
@@ -72,7 +72,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Multiply_Null()
+        public void Multiply_Null_Luu()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -80,7 +80,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Divide_Valid()
+        public void Divide_Valid_Luu()
         {
             Assert.AreEqual(3, Program.Divide("6", "2"));
             Assert.AreEqual(5, Program.Divide("10", "2"));
@@ -88,7 +88,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Divide_Invalid()
+        public void Divide_Invalid_Luu()
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
@@ -96,7 +96,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Divide_Null()
+        public void Divide_Null_Luu()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -104,7 +104,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Power_Valid()
+        public void Power_Valid_Luu()
         {
             Assert.AreEqual(9, Program.Power("3", "2"));
             Assert.AreEqual(49, Program.Power("7", "2"));
@@ -112,7 +112,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Power_Invalid()
+        public void Power_Invalid_Luu()
         {
             Assert.Throws<FormatException>(() => Program.Power("1", "a"));
             Assert.Throws<FormatException>(() => Program.Power("a", "1"));
@@ -120,7 +120,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Power_Null()
+        public void Power_Null_Luu()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
